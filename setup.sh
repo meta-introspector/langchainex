f0507872b9c3821411d69a913a37381d12507142
Ubuntu <ubuntu@t1v-n-3a86b962-w-0.europe-west4-a.c.trc-tpu-project.internal>
2024-03-05 21:10:23 +0000

    setup

---
+++ setup.sh
@@ -0,0 +1,11 @@
+sudo NEEDRESTART_MODE=a apt install -y libncurses5-dev autoconf automake libssl-dev
+asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
+asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
+asdf install elixir 1.15.4-otp-26 
+asdf install erlang 26.0.2
+asdf global erlang 26.0.2
+asdf global elixir 1.14.3
+asdf install elixir 1.14.3
+mix install
+mix deps.get
+mix 
