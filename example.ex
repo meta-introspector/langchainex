69c48ccf66c7a64f1a6d8b6efaf0f546e82e9f5c
Ubuntu <ubuntu@t1v-n-cbea58e8-w-0.europe-west4-a.c.trc-tpu-project.internal>
2024-03-05 19:54:07 +0000

    run all

--- example.ex
+++ example.ex
@@ -1,3 +1,18 @@
 # iex -S mix run
-model = %LangChain.Providers.Bumblebee.LanguageModel{    model_name: "gpt2"  }
-response = LangChain.LanguageModelProtocol.ask(model, "Why is Elixir a good language for AI applications?")
+
+"../bloke/top100.csv"
+    |> Path.expand(__DIR__)
+    |> File.stream!
+    |> Stream.drop(1)
+    |> Stream.map(&String.trim(&1, "\n"))
+    |> Stream.map(&String.split(&1, ","))
+    |> Enum.map(
+    fn(pairs) ->
+    val = List.first(pairs)
+    IO.puts val
+    model = %LangChain.Providers.Bumblebee.LanguageModel{    model_name: val  }
+    response = LangChain.LanguageModelProtocol.ask(model, "Why is Elixir a good language for AI applications?")
+    IO.puts response
+    response = LangChain.LanguageModelProtocol.ask(model, "Why is Elixir a good language for AI applications? Because [MASK]")
+    IO.puts response
+end)
